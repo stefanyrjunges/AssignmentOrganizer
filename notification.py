3cf1a312601b8d8083de3d5c8be1f4aad2f30d5b
Stefany Rachor Junges <148558645+stefanyrjunges@users.noreply.github.com>
2024-02-13 16:50:58 +0000

    Update notification.py

--- notification.py
+++ notification.py
@@ -2,7 +2,7 @@ import openpyxl
 from datetime import datetime, date
 import requests
 
-workbook = openpyxl.load_workbook('data.xlsx')
+workbook = openpyxl.load_workbook('NAME OF THE EXCEL FILE ONLY -> example: data.xlsx') 
 sheet = workbook.active
 date_column = sheet['C']
 assg_column = sheet['A']
@@ -21,15 +21,15 @@ for cell, cell_assignment_name in zip(date_column, assg_column):
             continue
 
         if cell_date_formatted == today_date_formatted:
-            # requests.post('https://api.mynotifier.app', {
-            # "apiKey":  '********-****-****-****-************',
-            # "description": "Hello! Your assignment {0} is due today ({1}). Don't forget to send it!".format(cell_assignment_name.value, cell_date_formatted),
-            # "message": "Assignment due today!",
-            # "type": "info"
-            # })
+            requests.post('https://api.mynotifier.app', {
+            "apiKey":  '********-****-****-****-************', # Add your own apiKey here
+            "description": "Hello! Your assignment {0} is due today ({1}). Don't forget to send it!".format(cell_assignment_name.value, cell_date_formatted),
+            "message": "Assignment due today!",
+            "type": "info"
+            })
             
-          
-            print(f"Hello! Your assignment {cell_assignment_name.value} is due today ({cell_date_formatted}). Don't forget to send it!")
+            # To test it without sending the notification:
+            # print(f"Hello! Your assignment {cell_assignment_name.value} is due today ({cell_date_formatted}). Don't forget to send it!")
 
     except ValueError:
         print(f"Skipping row {cell.row} - Invalid date format: {cell.value}")
